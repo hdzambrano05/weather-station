0e37ecc9ac1d976fbcb9834daec75c3bb226a4cb
Harold Zambrano <hdzambrano.2350@unicesmag.edu.co>
2024-09-09 21:55:19 -0500

    Version 1.0

---
+++ api/database.py
@@ -0,0 +1,37 @@
+'''
+Dev: Harold Zambrano
+Script description: wheather station
+Engine: SQLite
+'''
+
+#Import engine database package
+import sqlite3
+
+#Create weather-station database conncetion
+con = sqlite3.connect('weather_station.db')
+
+#Crate cursor
+cur = con.cursor()
+
+#Users model
+
+user_model = ''' 
+    CREATE TABLE IF NOT EXISTS users (
+        id INTERGER PRIMARY KEY,
+        username TEXT NOT NULL,
+        email TEXT NOT NULL,
+        password TEXT NOT NULL,
+        role TEXT NOT NULL,
+        status BOOLEAN DEFAULT true,
+        created_at TIMESRAMP DEFAULT (datetime ('now', 'localtime')),
+        update_at TIMESRAMP DEFAULT (datetime ('now', 'localtime')),
+        deleted_at null
+    )
+'''
+
+#Execute query
+cur.execute(user_model)
+
+#Close connection
+con.close
+
